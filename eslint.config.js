3d7de85e20a6537eec43899913d44077d17bd2c3
Rowan Twell <rowantwell@gmail.com>
2025-06-10 10:55:09 -0600

    ignore playwright, fix eslint

--- eslint.config.js
+++ eslint.config.js
@@ -70,8 +70,7 @@ export default [
     rules: {
       ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
-      "no-unused-vars": "off",
-      "@typescript-eslint/no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
+      "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
       "react-refresh/only-export-components": [
         "warn",
         { allowConstantExport: true },
